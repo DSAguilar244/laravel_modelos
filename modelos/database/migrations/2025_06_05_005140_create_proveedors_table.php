4c9edca4ffe5b4cd8e2968027b3959bea5da5f64
DSAguilar244 <alexisbryan1924@gmail.com>
2025-06-04 21:31:51 -0500

    upfixproblems

--- modelos/database/migrations/2025_06_05_005140_create_proveedors_table.php
+++ modelos/database/migrations/2025_06_05_005140_create_proveedors_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('proveedores', function (Blueprint $table) {
+        Schema::create('proveedors', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
             $table->string('direccion');
@@ -20,6 +20,6 @@ return new class extends Migration
 
     public function down(): void
     {
-        Schema::dropIfExists('proveedores');
+        Schema::dropIfExists('proveedors');
     }
 };
